6bf0fc86618db6346c64227f4f9fed06166d9bf9
Noah Gibson <135noahg@gmail.com>
2018-10-27 19:01:17 -0400

    added react-native-firebase to app

--- android/app/src/main/java/com/teamgobbu/reliefnet/MainApplication.java
+++ android/app/src/main/java/com/teamgobbu/reliefnet/MainApplication.java
@@ -1,9 +1,10 @@
-package com.invertase.rnfirebasestarter;
+package com.teamgobbu.reliefnet;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -43,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
         new MainReactPackage(),
+            new RNFirebasePackage(),
         new RNFirebasePackage(),
         // add/remove these packages as appropriate
         new RNFirebaseAdMobPackage(),
